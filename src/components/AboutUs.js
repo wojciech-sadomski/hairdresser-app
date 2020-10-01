4322fb32e4b007b5e28b16172a161f97bb7103aa
Wojciech Sadomski <wsadomski@gmail.com>
2020-10-01 09:27:52 +0200

    about us style

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -7,13 +7,39 @@ const AboutUs = () => {
       <NavLink className="aboutUs__back" to="/">
         Home
       </NavLink>
-      <img src="" alt="" className="AboutUs__foto" />
+      <div className="aboutUs__foto"></div>
       <div className="aboutUs__logo"></div>
       <div className="aboutUs__description">
-        <h1 className="aboutUs__description__header">Something about us</h1>
-        <p className="aboutUs__description__text"></p>
-        <p></p>
-        <p></p>
+        <h1 className="aboutUs__description__header">ABOUT US</h1>
+        <div></div>
+        <p className="aboutUs__description__text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias,
+          natus. Sequi, mollitia vitae. Enim molestiae animi, quo iusto
+          excepturi, impedit expedita quas aut eaque iure veniam non aliquam
+          harum quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quasi incidunt provident ducimus ipsum similique, omnis magni aliquid,
+          ea labore tenetur, laboriosam consequuntur. Dolor, beatae sunt. Minima
+          sit deserunt voluptatibus voluptas.
+        </p>
+        <p className="aboutUs__description__text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel odio
+          numquam, quidem magnam voluptate temporibus delectus sit fuga deleniti
+          porro soluta voluptates labore facilis quos asperiores exercitationem
+          a ducimus magni. Lorem ipsum dolor sit, amet consectetur adipisicing
+          elit. Quidem reiciendis minus quo repellendus cum neque sint ad sunt
+          provident sit rerum obcaecati corporis, modi tempore temporibus fuga.
+          Sint, voluptates eos.
+        </p>
+        <p className="aboutUs__description__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          quibusdam harum nostrum itaque, possimus esse perspiciatis
+          consequatur, quidem aspernatur rem, magnam dicta voluptatem vitae
+          quaerat asperiores consequuntur hic quam. Magnam? Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Accusamus laborum modi aperiam
+          possimus laudantium sapiente obcaecati, nemo repudiandae eius. Placeat
+          tempora aperiam eos voluptate doloremque enim. Ratione, natus ex!
+          Autem.
+        </p>
       </div>
     </div>
   );
