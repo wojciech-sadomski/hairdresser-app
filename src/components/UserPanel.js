481fb3dfa4bbf6db3acf38c66578fb50a6dc1963
Wojciech Sadomski <wsadomski@gmail.com>
2020-10-06 13:54:39 +0200

    static user panel

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -9,16 +9,19 @@ class UserPanel extends React.Component {
           Home
         </NavLink>
         <h1 className="userPanel__header">Hairdresser</h1>
-        <div className="userPanel--data">
-          <input type="date" />
+        <div className="userPanel__info">
+          <div className="userPanel__info userPanel__info--center">
+            <input type="date" />
+          </div>
         </div>
-
-        <div className="userPanel--visits">
-          <ul>
-            <li>Balejaż</li>
-            <li>Czesanie</li>
-            <li>Farbowanie</li>
-          </ul>
+        <div className="userPanel__info">
+          <div className="userPanel__info--top">
+            <ul>
+              <li>Balejaż</li>
+              <li>Czesanie</li>
+              <li>Farbowanie</li>
+            </ul>
+          </div>
         </div>
       </div>
     );
