6285ee3506b34a9138a5ac5b6566557bf97ce1cf
Wojciech Sadomski <wsadomski@gmail.com>
2020-10-07 12:03:26 +0200

    Updates

---
+++ precache-manifest.a10fd0714f099bfded48acc02210d740.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51dca1c67ba791984a0ffed2a900fe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b9804aea1bd453906b8",
+    "url": "/static/css/main.431fc51f.chunk.css"
+  },
+  {
+    "revision": "0f90d2200d5678cd723d",
+    "url": "/static/js/2.5d5773b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5d5773b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9804aea1bd453906b8",
+    "url": "/static/js/main.3a002264.chunk.js"
+  },
+  {
+    "revision": "95b38cbb013c947d7f0f",
+    "url": "/static/js/runtime-main.64a80552.js"
+  },
+  {
+    "revision": "e272fdb9419873e3b5733947f0febeea",
+    "url": "/static/media/about-us-decoration.e272fdb9.png"
+  },
+  {
+    "revision": "8707817c2093e1ab96a117eb947128d2",
+    "url": "/static/media/home-background.8707817c.jpg"
+  }
+]);
